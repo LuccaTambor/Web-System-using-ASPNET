27174456e7b923ab42003a8f873f37f21feaa849
LuccaTambor <lucca.tambor@outlook.com>
2022-02-18 12:04:10 -0300

    Add department model and its initial view

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWebMVC.Models.ViewModels;
 
 namespace SalesWebMVC.Controllers
 {
@@ -17,7 +18,8 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App Project.";
+            ViewData["Owner"] = "Lucca Augusto Torres Tambor";
 
             return View();
         }
